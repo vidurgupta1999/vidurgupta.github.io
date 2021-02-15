1fa547ea093378c02b0d03db8f968864a052148b
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2021-02-15 00:07:54 -0500

    Update Name.js

--- src/Name.js
+++ src/Name.js
@@ -10,11 +10,11 @@ const greeting = {
     Programming: true,  // if true, Show 'Programming' section in home page, if false than not show
     developer: true,  // if true, Show 'Full Stack Development' section in home page, if false than not show
     designer: true,  // if true, Show 'designer' section in home page, if false than not show
-    cloud_infra_architecture:true,  // if true, Show 'cloud_infra_architecture' section in home page, if false than not show
+    cloud_infra_architecture:false,  // if true, Show 'cloud_infra_architecture' section in home page, if false than not show
     data_Science: true,  // if true, Show 'data_Science' section in home page, if false than not show
-    ml_ai: true,  // if true, Show 'ml_ai' section in home page, if false than not show
-    Robotic: true,  // if true, Show 'Robotic' section in home page, if false than not show
-    gamedev: true,  // if true, Show 'gamedev' section in home page, if false than not show
+    ml_ai: false,  // if true, Show 'ml_ai' section in home page, if false than not show
+    Robotic: false,  // if true, Show 'Robotic' section in home page, if false than not show
+    gamedev: false,  // if true, Show 'gamedev' section in home page, if false than not show
 };
 
 const BlogData = {
