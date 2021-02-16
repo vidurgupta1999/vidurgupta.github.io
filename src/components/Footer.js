32d5aef9a2abcda8e3c540b6e795fa988df9f2ed
vidurgupta2 <70779106+vidurgupta2@users.noreply.github.com>
2021-02-15 19:14:41 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 4bee324... Revert "Update Footer.js"
 import React, { Component } from "react";
 import { greeting } from '../Name';
 import './footer.css'
 
 class Footer extends Component {
     render() {
+<<<<<<< HEAD
       console.log("Theme by Master-Portfolio  https://github.com/vimalverma558/Master-Portfolio")
+=======
+      console.log("")
+>>>>>>> parent of 4bee324... Revert "Update Footer.js"
       return (
         <div className="foot">
           
@@ -16,13 +23,21 @@ class Footer extends Component {
           https://github.com/vimalverma558/Master-Portfolio */}
 
 
+<<<<<<< HEAD
           <p>Made with <a href="https://github.com/vimalverma558/Master-Portfolio">Master-Portfolio</a> <span role="img" aria-label="Heart"> ❤️ </span> by { greeting.full_name } </p>
+=======
+          <p></p>
+>>>>>>> parent of 4bee324... Revert "Update Footer.js"
           
         </div>
       );
     }
   }
   
+<<<<<<< HEAD
   export default Footer;
 =======
 >>>>>>> parent of e42b7f8... Update Footer.js
+=======
+  export default Footer;
+>>>>>>> parent of 4bee324... Revert "Update Footer.js"
